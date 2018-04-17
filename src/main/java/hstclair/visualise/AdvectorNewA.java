1c4eb57a43c35b3fc39ed86a55db9b2a4f5cf7d6
Hal St. Clair <hdstclair@gmail.com>
2018-04-16 20:35:17 -0500

    Does not build currently.  Preparing to test old and new code side-by-side but first need to fix call to Advector.advect in FluidSolverA and then add code or test cases to compare processing results for each stage.

---
+++ src/main/java/hstclair/visualise/AdvectorNewA.java
@@ -0,0 +1,360 @@
+package hstclair.visualise;
+
+import hstclair.visualise.grid.DoubleGrid;
+
+public class AdvectorNewA implements Advector {
+
+    FluidSolver solver;
+    int edgeLength;
+    int size;
+    int rowSize;
+    int[] rowOffset;
+
+    public AdvectorNewA(FluidSolverA solver, int edgeLength, int size) {
+
+        this.solver = solver;
+        this.edgeLength = edgeLength;
+        this.size = size;
+        this.rowSize = edgeLength + 2;
+
+        rowOffset = new int[edgeLength << 1];
+
+        for (int i = 0; i < rowOffset.length; i++) {
+            int offset = i - edgeLength;
+
+            rowOffset[i] = offset * rowSize;
+        }
+
+    }
+
+    public void advect1(int b, double[] d, double[] d0, double[] du, double[] dv, double dt) {
+        double dt0;
+
+        dt0 = dt * edgeLength;
+
+        for (int col = 1; col <= edgeLength; col++) {
+            int rowNum = 1;
+
+            for (int rowIndex = rowSize; rowIndex < size - rowSize; rowIndex += rowSize) {
+
+                int index = rowIndex + col;
+
+                double dx = dt0 * du[index];
+                double dy = dt0 * dv[index];
+
+                int dxi = (int) dx;
+                int dyi = (int) dy;
+
+                int x0 = col - dxi;
+                int y0 = rowNum - dyi;
+
+                double dxf = dx - dxi;
+                double dyf = dy - dyi;
+
+
+                if (x0 >= edgeLength) {
+
+                    x0 = edgeLength;
+
+                    if (dxf > .5)
+                        dxf = .5;
+                } else if ( x0 <= 1) {
+                    x0 = 1;
+
+                    if (dxf < -.5)
+                        dxf = -.5;
+                }
+
+                if (y0 >= edgeLength - 1) {
+
+                    y0 = ((y0 - 1) % edgeLength) + 1;
+
+                } else if ( y0 < 0) {
+
+                    y0 = ((y0 - 1) % edgeLength) + edgeLength + 1;
+                }
+
+//                int i0 = (int) x0;
+                int x1 = x0 + 1;
+
+                int y1 = y0 + 1;
+
+                if (x1 > edgeLength + 1) x1 = edgeLength + 1;
+                if (x1 <= 0) x1 = 0;
+
+                if (y1 > edgeLength) y1 -= edgeLength;
+                if (y1 < 0) y1 += edgeLength;
+
+                int dx0 = x0 - col;
+                int dy0 = y0 - rowNum;
+                int dx1 = x1 - col;
+                int dy1 = y1 - rowNum;
+
+                double s1 = dxf % 1;
+                double s0 = 1 - s1;
+                double t1 = dyf % 1;
+                double t0 = 1 - t1;
+
+
+                int index00 = index + dx0 + rowOffset[edgeLength + dy0];
+                int index01 = index + dx0 + rowOffset[edgeLength + dy1];
+                int index10 = index + dx1 + rowOffset[edgeLength + dy0];
+                int index11 = index + dx1 + rowOffset[edgeLength + dy1];
+
+                d[index] = s0 * (t0 * d0[index00] + t1 * d0[index01]) + s1 * (t0 * d0[index10] + t1 * d0[index11]);
+
+                rowNum++;
+            }
+        }
+    }
+
+
+    static class AdvectFunction {
+
+        double[] d;
+        double[] d0;
+
+        boolean reference;
+        boolean validate;
+
+        int index00;
+        int index01;
+        int index10;
+        int index11;
+
+        double s0;
+        double t0;
+        double s1;
+        double t1;
+
+        public AdvectFunction(double[] d, double[] d0) {
+            this.d = d;
+            this.d0 = d0;
+        }
+
+        void reset() {
+            validate = false;
+            reference = false;
+        }
+
+        void setReference() {
+            reference = true;
+            validate = false;
+        }
+
+        void setValidate() {
+            validate = true;
+            reference = false;
+        }
+
+
+        void apply(int index, int index00, int index01, int index10, int index11, double s0, double t0, double s1, double t1) {
+
+            if (reference) {
+
+                this.index00 = index00;
+                this.index01 = index01;
+                this.index10 = index10;
+                this.index11 = index11;
+
+                this.s0 = s0;
+                this.t0 = t0;
+                this.s1 = s1;
+                this.t1 = t1;
+            } else if (validate) {
+
+                if (this.index00 != index00 || this.index01 != index01 || this.index10 != index10 || this.index11 != index11
+                    || this.s0 != s0 || this.t0 != t0 || this.s1 != s1 || this.t1 != t1)
+                    throw new RuntimeException();
+            }
+
+            if (! validate)
+                d[index] = s0 * (t0 * d0[index00] + t1 * d0[index01]) + s1 * (t0 * d0[index10] + t1 * d0[index11]);
+        }
+    }
+
+
+    void advectCoreOrg(double dx, double dy, int col, int rowNum, int index, AdvectFunction advectFunction) {
+
+        double x0 = col - dx;
+        double y0 = rowNum - dy;
+
+        if (x0 > edgeLength + 0.5) x0 = edgeLength + 0.5f;
+        if (x0 < 0.5)     x0 = .5;
+
+        if (y0 >= edgeLength - 1) y0 = ((y0 - 1) % edgeLength) + 1;
+        if (y0 < 0) y0 = ((y0 - 1) % edgeLength) + edgeLength + 1;
+
+        int i0 = (int) x0;
+        int i1 = i0 + 1;
+
+        int j0 = (int) y0;
+        int j1 = j0 + 1;
+
+        if (i1 > edgeLength + 1) i1 = edgeLength + 1;
+        if (i1 <= 0) i1 = 0;
+
+        if (j1 > edgeLength) j1 -= edgeLength;
+        if (j1 < 0) j1 += edgeLength;
+
+        int dx0i = i0 - col;
+        int dy0i = j0 - rowNum;
+        int dx1i = i1 - col;
+        int dy1i = j1 - rowNum;
+
+        double s1 = x0 - (int) x0;
+        double s0 = 1 - s1;
+        double t1 = y0 - (int) y0;
+        double t0 = 1 - t1;
+
+        int index00 = index + dx0i + rowOffset[edgeLength + dy0i];
+        int index01 = index + dx0i + rowOffset[edgeLength + dy1i];
+        int index10 = index + dx1i + rowOffset[edgeLength + dy0i];
+        int index11 = index + dx1i + rowOffset[edgeLength + dy1i];
+
+        advectFunction.apply(index, index00, index01, index10, index11, s0, t0, s1, t1);
+    }
+
+    void advectCoreNew(double dx, double dy, int col, int rowNum, int index, AdvectFunction advectFunction) {
+
+        int dxi = (int) dx;
+        int dyi = (int) dy;
+
+        double x0 = col - dx;
+        double y0 = rowNum - dy;
+
+        int x0i = (int) x0;
+        int y0i = (int) y0;
+
+        double dxf = x0 - x0i;
+        double dyf = y0 - y0i;
+
+
+        if (x0i > edgeLength || x0i == edgeLength && dxf > .5) {
+
+            x0i = edgeLength;
+
+            dxf = .5;
+        } else if ( x0i < 0 || x0i == 0 && dxf < .5) {
+            x0i = 0;
+
+            dxf = .5;
+        }
+
+        if (y0i >= edgeLength - 1) {
+
+            y0i = ((y0i - 1) % edgeLength) + 1;
+
+        } else if ( y0i < 0) {
+
+            y0i = ((y0i - 1) % edgeLength) + edgeLength + 1;
+        }
+
+//                int i0 = (int) x0;
+        int x1 = x0i + 1;
+
+        int y1 = y0i + 1;
+
+        if (x1 > edgeLength + 1) x1 = edgeLength + 1;
+        if (x1 <= 0) x1 = 0;
+
+        if (y1 > edgeLength) y1 -= edgeLength;
+        if (y1 < 0) y1 += edgeLength;
+
+        int dx0 = x0i - col;
+        int dy0 = y0i - rowNum;
+        int dx1 = x1 - col;
+        int dy1 = y1 - rowNum;
+
+        double s1 = dxf % 1;
+        double s0 = 1 - s1;
+        double t1 = dyf % 1;
+        double t0 = 1 - t1;
+
+
+        int index00 = index + dx0 + rowOffset[edgeLength + dy0];
+        int index01 = index + dx0 + rowOffset[edgeLength + dy1];
+        int index10 = index + dx1 + rowOffset[edgeLength + dy0];
+        int index11 = index + dx1 + rowOffset[edgeLength + dy1];
+
+
+        try {
+            advectFunction.apply(index, index00, index01, index10, index11, s0, t0, s1, t1);
+        } catch (Exception ex) {
+            throw ex;
+        }
+    }
+
+    @Override
+    public void advect(int b, DoubleGrid d, DoubleGrid d0, DoubleGrid du, DoubleGrid dv, double dt) {
+        advect(b, d.grid, d0.grid, du.grid, dv.grid, dt);
+    }
+
+    public void advect(int b, double[] d, double[] d0, double[] du, double[] dv, double dt) {
+        double dt0;
+
+        AdvectFunction advectFunction = new AdvectFunction(d, d0);
+
+        dt0 = dt * edgeLength;
+
+        for (int col = 1; col <= edgeLength; col++) {
+            int rowNum = 1;
+
+            for (int rowIndex = rowSize; rowIndex < size - rowSize; rowIndex += rowSize) {
+
+                int index = rowIndex + col;
+
+                double dx = dt0 * du[index];
+                double dy = dt0 * dv[index];
+
+//                advectFunction.setReference();
+//
+//                advectCoreOrg(dx, dy, col, rowNum, index, advectFunction);
+//
+//                advectFunction.setValidate();
+
+                advectCoreNew(dx, dy, col, rowNum, index, advectFunction);
+
+//                double x0 = col - dx;
+//                double y0 = rowNum - dy;
+//
+//                if (x0 > edgeLength + 0.5) x0 = edgeLength + 0.5f;
+//                if (x0 < 0.5)     x0 = .5;
+//
+//                if (y0 >= edgeLength - 1) y0 = ((y0 - 1) % edgeLength) + 1;
+//                if (y0 < 0) y0 = ((y0 - 1) % edgeLength) + edgeLength + 1;
+//
+//                int i0 = (int) x0;
+//                int i1 = i0 + 1;
+//
+//                int j0 = (int) y0;
+//                int j1 = j0 + 1;
+//
+//                if (i1 > edgeLength + 1) i1 = edgeLength + 1;
+//                if (i1 <= 0) i1 = 0;
+//
+//                if (j1 > edgeLength) j1 -= edgeLength;
+//                if (j1 < 0) j1 += edgeLength;
+//
+//                int dx0i = i0 - col;
+//                int dy0i = j0 - rowNum;
+//                int dx1i = i1 - col;
+//                int dy1i = j1 - rowNum;
+//
+//                double s1 = x0 - (int) x0;
+//                double s0 = 1 - s1;
+//                double t1 = y0 - (int) y0;
+//                double t0 = 1 - t1;
+//
+//                int index00 = index + dx0i + rowOffset[edgeLength + dy0i];
+//                int index01 = index + dx0i + rowOffset[edgeLength + dy1i];
+//                int index10 = index + dx1i + rowOffset[edgeLength + dy0i];
+//                int index11 = index + dx1i + rowOffset[edgeLength + dy1i];
+//
+//                d[index] = s0 * (t0 * d0[index00] + t1 * d0[index01]) + s1 * (t0 * d0[index10] + t1 * d0[index11]);
+
+                rowNum++;
+            }
+        }
+    }
+}
---
+++ src/main/java/hstclair/visualise/FluidSolverA.java
@@ -0,0 +1,472 @@
+package hstclair.visualise;
+
+import java.util.Random;
+
+/**
+ * Jos Stam style fluid solver with vorticity confinement
+ * and buoyancy force.
+ *
+ * @author Alexander McKenzie
+ * @version 1.0
+ **/
+
+public class FluidSolverA
+{
+    Random random = new Random(System.currentTimeMillis());
+
+    int row;
+    int edgeLength, size;
+    int nSquared;
+
+    //    float visc = 0;
+//    float visc = 0.000085f;
+//    float visc = 0.00015f;
+//    double visc = .00008;   // non-jupiter
+    double visc = .000002;   // jupiter
+    double diff = 0.0d;
+
+    double[] tmp;
+
+    double[] density, densityOld;
+    double[] u, uOld;
+    double[] v, vOld;
+    double[] curl;
+
+    Advector advector;
+
+    double meanDensity;
+    double maxDensity;
+    double minDensity;
+
+    int repeats = 20;
+
+
+    public FluidSolverA(int edgeLength, double dt, double viscosity, double diffusion) {
+        setup(edgeLength);
+        visc = viscosity;
+        diff = diffusion;
+    }
+
+    /**
+     * Set the grid size.
+     **/
+    public void setup(int n)
+    {
+        this.edgeLength = n;
+        this.nSquared = n*n;
+        this.row = n+2;
+        size = (n + 2) * (n + 2);
+
+        advector = new AdvectorNewA(this, edgeLength, size);
+
+        reset();
+    }
+
+
+    /**
+     * Reset the datastructures.
+     * We use 1d arrays for speed.
+     **/
+
+    public void reset()
+    {
+        density = new double[size];
+        densityOld = new double[size];
+        u    = new double[size];
+        uOld = new double[size];
+        v    = new double[size];
+        vOld = new double[size];
+        curl = new double[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            u[i] = uOld[i] = v[i] = vOld[i] = 0.0f;
+            density[i] = densityOld[i] = curl[i] = 0.0f;
+        }
+    }
+
+
+    /**
+     * Calculate the buoyancy force as part of the velocity solver.
+     * Fbuoy = -a*d*Y + b*(T-Tamb)*Y where Y = (0,1). The constants
+     * a and b are positive with appropriate (physically meaningful)
+     * units. T is the temperature at the current cell, Tamb is the
+     * average temperature of the fluid grid. The density d provides
+     * a mass that counteracts the buoyancy force.
+     *
+     * In this simplified implementation, we say that the tempterature
+     * is synonymous with density (since smoke is *hot*) and because
+     * there are no other heat sources we can just use the density
+     * field instead of a new, seperate temperature field.
+     *
+     * @param Fbuoy Array to store buoyancy force for each cell.
+     **/
+    public void buoyancy(double [] Fbuoy)
+    {
+        double ambientTemperature = 0;
+        double a = 0.000625f;
+        double b = 0.025f;
+
+        // sum all temperatures
+        for (int rowIndex = row; rowIndex <= size - row; rowIndex += row) {
+            for (int col = 1; col <= edgeLength; col++) {
+                ambientTemperature += density[rowIndex + col];
+            }
+        }
+
+        // get average temperature
+        ambientTemperature /= nSquared;
+
+        double q = b*ambientTemperature;
+        double p = a-b;
+
+        // for each cell compute buoyancy force
+        for (int rowIndex = row; rowIndex <= size - row; rowIndex += row) {
+            for (int col = 1; col < edgeLength; col++) {
+                int index = rowIndex + col;
+//                Fbuoy[index] = a * d[index] -b * (d[index] - ambientTemperature);
+
+                // fbuoy = a*x - b*(x-amb)
+                // fbuoy = a*x -b*x + b*amb
+                // fbuoy = (a-b)*x + b*amb
+
+                // p = a-b
+                // q = b*amb
+
+                // fbuoy = p*x + q
+
+                Fbuoy[index] = p* density[index] + q;
+            }
+        }
+    }
+
+
+    /**
+     * Calculate the curl at position (i, j) in the fluid grid.
+     * Physically this represents the vortex strength at the
+     * cell. Computed as follows: w = (del x U) where U is the
+     * velocity vector at (i, j).
+     *
+     * @param index the index of the cell we're working with
+     **/
+
+    public double curl(int index /* int i, int j */)
+    {
+
+        double uDiff = u[index + row] - u[index - row];
+        double vDiff = v[index + 1] - v[index - 1];
+
+        return (uDiff-vDiff) * .5;
+    }
+
+
+    /**
+     * Calculate the vorticity confinement force for each cell
+     * in the fluid grid. At a point (i,j), Fvc = N x w where
+     * w is the curl at (i,j) and N = del |w| / |del |w||.
+     * N is the vector pointing to the vortex center, hence we
+     * add force perpendicular to N.
+     *
+     * @param Fvc_x The array to store the x component of the
+     *        vorticity confinement force for each cell.
+     * @param Fvc_y The array to store the y component of the
+     *        vorticity confinement force for each cell.
+     **/
+
+    public void vorticityConfinement(double[] Fvc_x, double[] Fvc_y)
+    {
+        double dw_dx, dw_dy;
+        double length;
+        double v;
+
+        int rowIndex = 0;
+        // Calculate magnitude of curl(u,v) for each cell. (|w|)
+        for (int i = 1; i <= edgeLength; i++) {
+            rowIndex += row;
+            for (int col = 1; col <= edgeLength; col++) {
+                int index = rowIndex + col;
+
+                curl[index] = Math.abs(curl(index));
+            }
+        }
+
+        int i = 0, j = 0;
+
+        for (rowIndex = row << 1; rowIndex < size - row; rowIndex += row) {
+            i++;
+            for (int col = 2; col < edgeLength; col++) {
+                j++;
+
+                int index = rowIndex + col;
+                // Find derivative of the magnitude (n = del |w|)
+                dw_dx = (curl[index + 1] - curl[index-1]) * 0.5f;
+                dw_dy = (curl[index + row] - curl[index-row]) * 0.5f;
+
+                // Calculate vector length. (|n|)
+                // Add small factor to prevent divide by zeros.
+                length = (float) Math.sqrt(dw_dx * dw_dx + dw_dy * dw_dy) + 0.000001f;
+
+                // N = ( n/|n| )
+                dw_dx /= length;
+                dw_dy /= length;
+
+                v = curl(rowIndex + col);
+
+                // N x w
+                Fvc_x[index] = dw_dy * -v;
+                Fvc_y[index] = dw_dx *  v;
+            }
+        }
+    }
+
+    public void applyDensity(int x, int y, double density) {
+        this.density[I(x, y)] += density;
+    }
+
+    public void applyForce(int x, int y, double dx, double dy) {
+        this.u[I(x, y)] += dx;
+        this.v[I(x, y)] += dy;
+    }
+
+    public void tick(double dt) {
+        velocitySolver(dt);
+        densitySolver(dt);
+    }
+
+
+    /**
+     * The basic velocity solving routine as described by Stam.
+     **/
+
+    public void velocitySolver(double dt)
+    {
+        // add velocity that was input by mouse
+        addSource(u, uOld, dt);
+        addSource(v, vOld, dt);
+
+        // add in vorticity confinement force
+        vorticityConfinement(uOld, vOld);
+        addSource(u, uOld, dt);
+        addSource(v, vOld, dt);
+
+        // add in buoyancy force
+        buoyancy(vOld);
+        addSource(v, vOld, dt);
+
+        // swapping arrays for economical mem use
+        // and calculating diffusion in velocity.
+        swapU();
+        diffuse(0, u, uOld, visc, dt);
+
+        swapV();
+        diffuse(0, v, vOld, visc, dt);
+
+        // we create an incompressible field
+        // for more effective advection.
+        project(u, v, uOld, vOld);
+
+        swapU(); swapV();
+
+        // self advect velocities
+        advect(1, u, uOld, uOld, vOld, dt);
+        advect(2, v, vOld, uOld, vOld, dt);
+
+        // make an incompressible field
+        project(u, v, uOld, vOld);
+
+        // clear all input velocities for next frame
+        for (int i = 0; i < size; i++){ uOld[i] = 0; vOld[i] = 0; }
+    }
+
+
+    /**
+     * The basic density solving routine.
+     **/
+
+    public void densitySolver(double dt)
+    {
+        // add density inputted by mouse
+        addSource(density, densityOld, dt);
+        swapD();
+
+        diffuse(0, density, densityOld, diff, dt);
+        swapD();
+
+        advect(0, density, densityOld, u, v, dt);
+
+        // clear input density array for next frame
+        for (int i = 0; i < size; i++) densityOld[i] = 0;
+
+        meanDensity = 0;
+        maxDensity = 0;
+
+        for (int i = 0; i < size; i++) {
+            double densityValue = density[i];
+            maxDensity = Math.max(maxDensity, densityValue);
+            minDensity = Math.min(minDensity, densityValue);
+            meanDensity += densityValue;
+        }
+
+        meanDensity /= size;
+
+    }
+
+    private void addSource(double[] x, double[] x0, double dt)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            x[i] += dt * x0[i];
+        }
+    }
+
+
+    /**
+     * Calculate the input array after advection. We start with an
+     * input array from the previous timestep and an and output array.
+     * For all grid cells we need to calculate for the next timestep,
+     * we trace the cell's center position backwards through the
+     * velocity field. Then we interpolate from the grid of the previous
+     * timestep and assign this value to the current grid cell.
+     *
+     * @param b Flag specifying how to handle boundries.
+     * @param d Array to store the advected field.
+     * @param d0 The array to advect.
+     * @param du The x component of the velocity field.
+     * @param dv The y component of the velocity field.
+     **/
+
+    private void advect(int b, double[] d, double[] d0, double[] du, double[] dv, double dt)
+    {
+        advector.advect(b, d, d0, du, dv, dt);
+
+        setBoundry(b, d);
+    }
+
+
+
+    /**
+     * Recalculate the input array with diffusion effects.
+     * Here we consider a stable method of diffusion by
+     * finding the densities, which when diffused backward
+     * in time yield the same densities we started with.
+     * This is achieved through use of a linear solver to
+     * solve the sparse matrix built from this linear system.
+     *
+     * @param b Flag to specify how boundries should be handled.
+     * @param c The array to store the results of the diffusion
+     * computation.
+     * @param c0 The input array on which we should compute
+     * diffusion.
+     * @param diff The factor of diffusion.
+     **/
+
+    private void diffuse(int b, double[] c, double[] c0, double diff, double dt)
+    {
+        double a = dt * diff * nSquared;
+        linearSolver(b, c, c0, a, 1 + 4 * a);
+    }
+
+
+    /**
+     * Use project() to make the velocity a mass conserving,
+     * incompressible field. Achieved through a Hodge
+     * decomposition. First we calculate the divergence field
+     * of our velocity using the mean finite differnce approach,
+     * and apply the linear solver to compute the Poisson
+     * equation and obtain a "height" field. Now we subtract
+     * the gradient of this field to obtain our mass conserving
+     * velocity field.
+     *
+     * @param x The array in which the x component of our final
+     * velocity field is stored.
+     * @param y The array in which the y component of our final
+     * velocity field is stored.
+     * @param p A temporary array we can use in the computation.
+     * @param div Another temporary array we use to hold the
+     * velocity divergence field.
+     *
+     **/
+
+    void project(double[] x, double[] y, double[] p, double[] div)
+    {
+        int halfN = (edgeLength >> 1);
+        double negOneOverTwoN = -0.5/ edgeLength;
+
+        for (int i = 1; i <= edgeLength; i++)
+        {
+            for (int j = 1; j <= edgeLength; j++)
+            {
+                int index = I(i,j);
+
+                div[index] = (x[index+1] - x[index-1]
+                        + y[index+row] - y[index-row])
+                        * negOneOverTwoN;
+                p[index] = 0;
+            }
+        }
+
+        setBoundry(0, div);
+        setBoundry(0, p);
+
+        linearSolver(0, p, div, 1, 4);
+
+        for (int i = 1; i <= edgeLength; i++)
+        {
+            for (int j = 1; j <= edgeLength; j++)
+            {
+                int index = I(i,j);
+
+                x[index] -= halfN * (p[index+1] - p[index-1]);
+                y[index] -= halfN * (p[index+row] - p[index-row]);
+            }
+        }
+
+        setBoundry(1, x);
+        setBoundry(2, y);
+    }
+
+
+    /**
+     * Iterative linear system solver using the Gauss-sidel
+     * relaxation technique. Room for much improvement here...
+     *
+     **/
+
+    void linearSolver(int b, double[] x, double[] x0, double a, double c)
+    {
+        int maxCol = edgeLength + 1;
+        int maxIndex = size - row;
+
+        for (int k = 0; k < repeats; k++) {
+            for (int col = 1; col < maxCol; col++) {
+                for (int index = row + col; index < maxIndex; index += row) {
+                    x[index] = (a * ( x[index-1] + x[index+1] + x[index- row] + x[index+ row]) + x0[index]) / c;
+                }
+            }
+            setBoundry(b, x);
+        }
+    }
+
+
+    // specifies simple boundry conditions for wrapping around a sphere east to west.
+    void setBoundry(int b, double[] x)
+    {
+        for (int i = 0; i <= edgeLength + 1; i++)
+        {
+            x[I(  i, 0  )] = x[I(i, edgeLength)];
+            x[I( i, edgeLength +1)] = + x[I(i,1)];
+        }
+    }
+
+    // util array swapping methods
+    public void swapU(){ tmp = u; u = uOld; uOld = tmp; }
+    public void swapV(){ tmp = v; v = vOld; vOld = tmp; }
+    public void swapD(){ tmp = density; density = densityOld; densityOld = tmp; }
+
+    public int INDEX(int x, int y) {
+        return I(x, y);
+    }
+
+    // util method for indexing 1d arrays
+    int I(int i, int j){ return i + (edgeLength + 2) * j; }
+}
